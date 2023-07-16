bf73940e3bdd0cad77ac9b71f1771e8231c8308f
abdullahshami9 <107029909+abdullahshami9@users.noreply.github.com>
2023-07-16 23:32:59 +0500

    crud completed

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -7,6 +7,27 @@ use App\models\listItem;
 
 class TodoListController extends Controller
 {
+    public function index(){
+        // return all values
+        // return view('welcome',['listitems' => listItem::all() ]);
+        
+        // return only conditional statements
+        return view('welcome',['listitems' => listItem::where('is_complete',0)->get() ]);
+    }
+
+    public function markComplete($id){
+        $li = listItem::find($id);
+        $li->is_complete = 1;
+        $li->save();
+        return redirect('/');
+    }
+
+    public function markDelete($id){
+        $li = listItem::find($id);
+        $li->delete();
+        return redirect('/');
+    }
+
     public function saveItem(Request $request)
 {
     // dd($request->listtag);
@@ -15,7 +36,8 @@ class TodoListController extends Controller
         $listItem->name = $request->listtag;
         $listItem->is_complete = 0;
         $listItem->save();
-        return view('welcome');
+
+        return redirect('/');
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use App\Http\Controllers\TodoListController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TodoListController::class,'index']);
+Route::post('/markCompleteRoute/{id}', [TodoListController::class,'markComplete'])->name('markComplete');
+Route::post('/markDeleteRoute/{id}', [TodoListController::class,'markDelete'])->name('markDelete');
+
 Route::post('/saveItemRoute', [TodoListController::class,'saveItem'])->name('saveItem');
 
